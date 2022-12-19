ec7344d25e51fc85446c3f6a321bc6666afa6829
francoglov <117660459+francoglov@users.noreply.github.com>
2022-12-19 08:55:49 +0800

    Add project files.

---
+++ VideoToThumbsAngularJS/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace VideoToThumbsAngularJS.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
